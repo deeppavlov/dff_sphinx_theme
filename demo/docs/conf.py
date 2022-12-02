6641bf2f6048ed4871857871e0ceae17593f78b9
pseusys <shveitsar215@gmail.com>
2022-12-03 02:32:17 +0300

    make rebuild documented, example thumbnail size fixed and right menu removed from thumbnail gallery

--- demo/docs/conf.py
+++ demo/docs/conf.py
@@ -32,8 +32,8 @@ exclude_patterns = ['**/README.rst']
 
 project = u'DFF Sphinx Theme Demo'
 copyright = u'DFF'
-version = "0.1.13"
-release = "0.1.13"
+version = "0.1.14"
+release = "0.1.14"
 language = 'en'
 
 pygments_style = 'default'
